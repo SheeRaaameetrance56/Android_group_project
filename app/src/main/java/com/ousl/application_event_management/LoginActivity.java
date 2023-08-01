674aa65b095fcda021d0dffd9dda9896124de639
Shehan wijeratne <shehanranjukaw5@gmail.com>
2023-08-01 13:15:47 +0530

    Account create UI

--- app/src/main/java/com/ousl/application_event_management/LoginActivity.java
+++ app/src/main/java/com/ousl/application_event_management/LoginActivity.java
@@ -2,13 +2,29 @@ package com.ousl.application_event_management;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button create_account_link;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Instantiate Elements
+        create_account_link = findViewById(R.id.create_account_navigation);
+
+        // Button functions
+        create_account_link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
