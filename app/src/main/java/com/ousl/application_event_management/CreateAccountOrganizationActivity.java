d2536a1567603827bd17377cdfe0e6b2e782da51
Shehan wijeratne <shehanranjukaw5@gmail.com>
2023-08-04 18:39:37 +0530

    CreateAccountOrganizationActivity UI and navigation

--- app/src/main/java/com/ousl/application_event_management/CreateAccountOrganizationActivity.java
+++ app/src/main/java/com/ousl/application_event_management/CreateAccountOrganizationActivity.java
@@ -2,13 +2,28 @@ package com.ousl.application_event_management;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateAccountOrganizationActivity extends AppCompatActivity {
 
+    Button navigation_sign_indvidual;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account_organization);
+
+
+        navigation_sign_indvidual = findViewById(R.id.navigation_sign_indvidual);
+
+        navigation_sign_indvidual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CreateAccountOrganizationActivity.this, CreateAccountActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
