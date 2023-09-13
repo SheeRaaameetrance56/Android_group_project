701daa45a51d002cfcc9c6bdda9e0d3105af0512
Shehan wijeratne <shehanranjukaw5@gmail.com>
2023-09-13 19:09:03 +0530

    Login authentication access and validation

--- app/src/main/java/com/ousl/application_event_management/LoginActivity.java
+++ app/src/main/java/com/ousl/application_event_management/LoginActivity.java
@@ -1,34 +1,75 @@
 package com.ousl.application_event_management;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.ousl.application_event_management.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
+    ActivityLoginBinding binding;
+    ProgressDialog progressDialog;
+    FirebaseAuth loginAuth;
+    FirebaseDatabase database;
+
+    String email, password;
     Button login_button;
     Button create_account_navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        login_button = findViewById(R.id.login_button);
-        create_account_navigation = findViewById(R.id.create_account_navigation);
+        loginAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
 
-        login_button.setOnClickListener(new View.OnClickListener() {
+        progressDialog = new ProgressDialog(LoginActivity.this);
+        progressDialog.setTitle("Login");
+        progressDialog.setMessage("Validation in progress.");
+
+        binding.loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, dashboard.class);
-                startActivity(intent);
+                email = binding.inputLoginEmail.getText().toString();
+                password = binding.inputLoginPassword.getText().toString();
+                if(!email.isEmpty() && !password.isEmpty()){
+                    progressDialog.show();
+                    loginAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            progressDialog.dismiss();
+                            if(task.isSuccessful()){
+                                Intent intent = new Intent(LoginActivity.this, dashboard.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                            else {
+                                Toast.makeText(LoginActivity.this, "Enter valid Email or Password", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+                else {
+                    Toast.makeText(LoginActivity.this, "Email and Password must required for Login", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
-        create_account_navigation.setOnClickListener(new View.OnClickListener() {
+        binding.createAccountNavigation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
