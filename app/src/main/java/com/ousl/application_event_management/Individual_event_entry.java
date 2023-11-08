40f7b737aad4e11e7c3eb20262a5bc17c92b340b
Shehan wijeratne <shehanranjukaw5@gmail.com>
2023-11-09 00:06:30 +0530

    PublicEventShowActivity Individual_event_entry.java making retrive data and save individual events

--- app/src/main/java/com/ousl/application_event_management/Individual_event_entry.java
+++ app/src/main/java/com/ousl/application_event_management/Individual_event_entry.java
@@ -1,14 +1,71 @@
 package com.ousl.application_event_management;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.ousl.application_event_management.databinding.ActivityIndividualEventEntryBinding;
+import com.ousl.application_event_management.models.PrivateEvents;
 
 public class Individual_event_entry extends AppCompatActivity {
+
+    ActivityIndividualEventEntryBinding binding;
+    String title, description, venue, date, time, limitations;
+    DatabaseReference reference;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_individual_event_entry);
+        binding = ActivityIndividualEventEntryBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        binding.priEventPublishBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                title = binding.priEventTitle.getText().toString();
+                description = binding.priEventDescription.getText().toString();
+                venue = binding.priEventVenue.getText().toString();
+                date = binding.priEventDate.getText().toString();
+                time = binding.priEventTime.getText().toString();
+                limitations = binding.priEventLimitations.getText().toString();
+
+                PrivateEvents privateEvent = new PrivateEvents(title,description,venue,date,time,limitations);
+
+                FirebaseUser currentUser = auth.getCurrentUser();
+                String uID = currentUser.getUid();
+                DatabaseReference userEventReference = reference.child("private_events").child(uID).push();
+
+                userEventReference.setValue(privateEvent).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()) {
+                            binding.priEventTitle.setText("");
+                            binding.priEventDescription.setText("");
+                            binding.priEventVenue.setText("");
+                            binding.priEventDate.setText("");
+                            binding.priEventTime.setText("");
+                            binding.priEventLimitations.setText("");
+                            Toast.makeText(Individual_event_entry.this, "Event added successfully", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(Individual_event_entry.this, "Something went wrong.!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/ousl/application_event_management/PublicEventShowActivity.java
+++ app/src/main/java/com/ousl/application_event_management/PublicEventShowActivity.java
@@ -1,16 +1,20 @@
 package com.ousl.application_event_management;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.ousl.application_event_management.databinding.ActivityPublicEventEntryBinding;
 import com.ousl.application_event_management.databinding.ActivityPublicEventShowBinding;
+import com.ousl.application_event_management.models.PublicEvent;
 
 public class PublicEventShowActivity extends AppCompatActivity {
     ActivityPublicEventShowBinding binding;
@@ -18,6 +22,8 @@ public class PublicEventShowActivity extends AppCompatActivity {
     TextView title, description, venue, date, time, limitations;
 
     FirebaseDatabase database;
+    FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,9 +37,39 @@ public class PublicEventShowActivity extends AppCompatActivity {
         time = binding.publicEventTimeView;
         limitations = binding.publicEventLimitationView;
 
-        database.getInstance();
-        DatabaseReference reference = database.getReference("https://database-splendy-default-rtdb.firebaseio.com/public_events");
+        database = FirebaseDatabase.getInstance();
+        auth = FirebaseAuth.getInstance();
 
+        // Get the current user's UID
+        String currentUserId = auth.getCurrentUser().getUid();
+
+        // Assuming you store the last entered event key in a variable called "lastEventKey"
+        String lastEventKey = "YOUR_LAST_EVENT_KEY";
+
+        DatabaseReference reference = database.getReference("public_events").child(currentUserId).child(lastEventKey);
+
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    // Retrieve event data and set it in your TextViews
+                    PublicEvent event = dataSnapshot.getValue(PublicEvent.class);
+                    if (event != null) {
+                        title.setText(event.getTitle());
+                        description.setText(event.getDescription());
+                        venue.setText(event.getVenue());
+                        date.setText(event.getDate());
+                        time.setText(event.getTime());
+                        limitations.setText(event.getLimitations());
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Handle any errors that occur.
+            }
+        });
 
         binding.dashboardButton.setOnClickListener(new View.OnClickListener() {
             @Override
