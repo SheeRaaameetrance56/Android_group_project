e9f744c399114280f116d794555d7fef0e37126b
Shehan wijeratne <shehanranjukaw5@gmail.com>
2023-11-14 14:17:53 +0530

    Improving dashboard with tab layout

---
+++ app/src/main/java/com/ousl/application_event_management/adapters/FragmentsAdapter.java
@@ -0,0 +1,4 @@
+package com.ousl.application_event_management.adapters;
+
+public class FragmentsAdapter {
+}
---
+++ app/src/main/java/com/ousl/application_event_management/dashboard_fragments/home/MyEventsFragment.java
@@ -0,0 +1,30 @@
+package com.ousl.application_event_management.dashboard_fragments.home;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ousl.application_event_management.R;
+import com.ousl.application_event_management.databinding.FragmentMyEventsBinding;
+
+public class MyEventsFragment extends Fragment {
+
+    private FragmentMyEventsBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentMyEventsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        //
+        return root;
+    }
+
+    public MyEventsFragment() {
+        // Required empty public constructor
+    }
+
+}
