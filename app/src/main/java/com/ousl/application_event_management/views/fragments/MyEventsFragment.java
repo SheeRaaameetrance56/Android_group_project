d0a951e3e4c15b4447568cbd28de6a63d11702e6
Shehan wijeratne <shehanranjukaw5@gmail.com>
2023-11-26 19:35:22 +0530

    Show listed private events

--- app/src/main/java/com/ousl/application_event_management/views/fragments/MyEventsFragment.java
+++ app/src/main/java/com/ousl/application_event_management/views/fragments/MyEventsFragment.java
@@ -1,29 +1,79 @@
 package com.ousl.application_event_management.views.fragments;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.ousl.application_event_management.databinding.FragmentMyEventsBinding;
+import com.ousl.application_event_management.models.PrivateEvents;
+import com.ousl.application_event_management.views.ListedEventsActivity;
+import com.ousl.application_event_management.views.adapters.PrivateEventAdapter;
 
 public class MyEventsFragment extends Fragment {
 
     private FragmentMyEventsBinding binding;
+    private PrivateEventAdapter privateEventAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentMyEventsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-        //
+
+        privateEventAdapter = new PrivateEventAdapter(requireContext());
+        binding.listedPrivateEvents.setLayoutManager(new GridLayoutManager(requireContext(),3));
+        getPrivateEvents();
         return root;
     }
 
     public MyEventsFragment() {
         // Required empty public constructor
+    }
+
+    public void getPrivateEvents(){
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = auth.getCurrentUser();
+
+        if (currentUser != null) {
+            String currentUserId = currentUser.getUid();
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference("private_events").child(currentUserId);
+
+            reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot eventsSnapshot) {
+                    for (DataSnapshot eventSnapshot : eventsSnapshot.getChildren()) {
+                        PrivateEvents event = eventSnapshot.getValue(PrivateEvents.class);
+                        privateEventAdapter.addEvent(event);
+                    }
+                    privateEventAdapter.notifyDataSetChanged();
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    // Handle database read error
+                    AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+                    builder.setMessage("Please check the mobile connection or WIFI")
+                            .setTitle("Connection Error");
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+            });
+        }
+
+        binding.listedPrivateEvents.setAdapter(privateEventAdapter);
     }
 
 }
